0205cc57ea2265e35b2b08d2a30ecfa35233a24c
OrfeasNikas03 <165455296+OrfeasNikas03@users.noreply.github.com>
2024-11-19 19:48:24 +0200

    potentially fixed gps code

--- src/Adafruit_ultimate_GPS.h
+++ src/Adafruit_ultimate_GPS.h
@@ -7,7 +7,7 @@ public:
 
     void begin() {
         //GPS hardware serial begin
-        Serial.begin(115200); 
+        Serial.begin(9600); 
         gpsSerial.begin(baudRate, SERIAL_8N1, rxPin, txPin);
         GPS.begin(baudRate);
         
@@ -20,15 +20,18 @@ public:
     } 
 
     //Boolean to only process data when there has been a change to the data
-    bool readData(){
+    bool readData() {
         char c = GPS.read();
-
-        if (GPS.newNMEAreceived()){
-            if (GPS.parse(GPS.lastNMEA())){
+        if (GPS.newNMEAreceived()) {
+            if (GPS.parse(GPS.lastNMEA())) {
                 return true;
+            } else {
+                Serial.println("Failed to parse NMEA sentence.");
             }
         }
         return false;
+    }
+
     }
     //Functions that get the GPS data
     float getLat() const { return GPS.latitude; }
@@ -36,13 +39,29 @@ public:
     float getAlt() const { return GPS.altitude; }
     float getSpeed() const { return GPS.speed; }
 
-    //GPS serial printing section of the code
     void printData() {
-        Serial.print("Latitude: "); Serial.println(getLat(), 4);
-        Serial.print("Longitude: "); Serial.println(getLong(), 4);
-        Serial.print("Altitude: "); Serial.println(getAlt());
-        Serial.print("Speed: "); Serial.println(getSpeed());
+        if (!GPS.fix) {
+            Serial.println("GPS:Waiting for GPS fix...");
+            return;
+        }
+        Serial.print("GPS:Latitude: "); Serial.println(getLat(), 4);
+        Serial.print("GPS:Longitude: "); Serial.println(getLong(), 4);
+        Serial.print("GPS:Altitude: "); Serial.println(getAlt());
+        Serial.print("GPS:Speed: "); Serial.println(getSpeed());
         Serial.println();
+    }
+
+    void setUpdateRate(int hz) {
+        if (hz == 1) {
+            GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+        } else if (hz == 5) {
+            GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);
+        } else if (hz == 10) {
+            GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ);
+        } else {
+            Serial.println("GPS:Unsupported update rate. Defaulting to 1 Hz.");
+            GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+        }
     }
 
 
